a353795e0ca94c0f3e1b2c62bb406034c8674482
Jose Chirivella <jose.chirivella@processmaker.com>
2024-01-22 14:32:09 -0600

    Removed the old cli plugins and replaced the old vue plugin with the new one to support vue 2.7.14

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import { createVuePlugin } from "vite-plugin-vue2";
+import vue from '@vitejs/plugin-vue2'
 import {resolve} from "path";
 
 
@@ -7,7 +7,7 @@ const libraryName = "VueMultiselect";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [createVuePlugin()],
+  plugins: [vue()],
   resolve: {
     alias: [
       {
